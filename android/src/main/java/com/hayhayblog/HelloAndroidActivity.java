42f1d31b45fc19501ef3bf607216e10a1581154e
Nguyen Duc Dung <dungvn3000@gmail.com>
2014-05-05 00:26:13 +0700

    Init.

--- android/src/main/java/com/hayhayblog/HelloAndroidActivity.java
+++ android/src/main/java/com/hayhayblog/HelloAndroidActivity.java
@@ -9,9 +9,10 @@ public class HelloAndroidActivity extends Activity {
 
     /**
      * Called when the activity is first created.
-     * @param savedInstanceState If the activity is being re-initialized after 
-     * previously being shut down then this Bundle contains the data it most 
-     * recently supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it is null.</b>
+     *
+     * @param savedInstanceState If the activity is being re-initialized after
+     *                           previously being shut down then this Bundle contains the data it most
+     *                           recently supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it is null.</b>
      */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -21,9 +22,9 @@ public class HelloAndroidActivity extends Activity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-	// Inflate the menu; this adds items to the action bar if it is present.
-	getMenuInflater().inflate(com.hayhayblog.R.menu.main, menu);
-	return true;
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(com.hayhayblog.R.menu.main, menu);
+        return true;
     }
 
 }
